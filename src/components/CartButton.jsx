dc936d79815dfc436a0bcb37abb7444f43a458ad
Karthickrampandian <114850859+Karthickrampandian@users.noreply.github.com>
2026-07-14 09:20:28 +0800

    SCRUM-11:Cart badge count does not update immediately when item is removed

--- src/components/CartButton.jsx
+++ src/components/CartButton.jsx
@@ -19,7 +19,13 @@ const CartButton = (props) => {
 
   useEffect(() => {
     ShoppingCart.registerCartListener(cartListener);
-  }, []);
+    return () => {
+      const index = ShoppingCart.LISTENERS.indexOf(cartListener);
+      if (index > -1) {
+        ShoppingCart.LISTENERS.splice(index, 1);
+      }
+    };
+  }, [cartListener]);
 
   if (cartContents.length > 0) {
     cartBadge = (
